3a1f0c3fce0696b50a1547cee3b53747db0c2a21
Priyanshi Morad <priyanshimorad17@gmail.com>
2024-07-12 18:08:33 +0530

    Add files via upload

---
+++ ch-7/7-1/7.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+	clrscr();
+	for(i=65 ; i<=69 ; i++)
+	{
+	    for(j=i ; j>=65 ; j--)
+	    {
+		printf("%c",j);
+	    }
+	    printf("\n");
+	}
+	getch();
+}
+
